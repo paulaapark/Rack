cfe4288f744d72a55e80b3a66fb1091f56fa2d41
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-12-17 00:20:32 -0500

    fixed the svg coat hanger icon

--- frontend/mobile/RackApp/src/app/tabs/tabs.page.ts
+++ frontend/mobile/RackApp/src/app/tabs/tabs.page.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
+  
 })
 export class TabsPage {
-
   constructor() {}
 
 }
