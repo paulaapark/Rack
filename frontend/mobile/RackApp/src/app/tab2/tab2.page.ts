f385dce7514af17861b0b8b6abdcc83d6b84d9ec
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-12-24 18:50:49 -0500

    added link to action sheet for image upload

--- frontend/mobile/RackApp/src/app/tab2/tab2.page.ts
+++ frontend/mobile/RackApp/src/app/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild} from '@angular/core';
 import { IonModal } from '@ionic/angular';
 
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -12,11 +14,39 @@ export class Tab2Page {
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
-
   confirm() {
     this.modal.dismiss(null, 'confirm');
   }
 
-  constructor() {}
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+    async presentActionSheet() {
+      const actionSheet = await this.actionSheetCtrl.create({
+        header: 'Do you wish to import or capture a new image?',
+        subHeader: 'Attach up to 3 images',
+        buttons: [
+          {
+            text: 'Import',
+            data: {
+              action: 'import',
+            },
+          },
+          {
+            text: 'Capture',
+            data: {
+              action: 'capture',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ],
+      });
+  
+      await actionSheet.present();
+    }
 
 }
