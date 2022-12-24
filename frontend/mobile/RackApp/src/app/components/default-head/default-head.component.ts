de6face50315ae1066aaa5f2b06f436a862fa3e5
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-12-24 18:54:21 -0500

    added camera action sheet for default header

--- frontend/mobile/RackApp/src/app/components/default-head/default-head.component.ts
+++ frontend/mobile/RackApp/src/app/components/default-head/default-head.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-default-head',
   templateUrl: './default-head.component.html',
@@ -7,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultHeadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+    async presentActionSheet() {
+      const actionSheet = await this.actionSheetCtrl.create({
+        header: 'Do you wish to import or capture a new image?',
+        subHeader: 'Attach up to 3 images for a new Rack item!',
+        buttons: [
+          {
+            text: 'Import',
+            data: {
+              action: 'import',
+            },
+          },
+          {
+            text: 'Capture',
+            data: {
+              action: 'capture',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ],
+      });
+  
+      await actionSheet.present();
+    }
 
   ngOnInit() {}
   
