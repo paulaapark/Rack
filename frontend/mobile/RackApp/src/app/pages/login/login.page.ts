51fa24c5ca90350a11c190259b796cc1b14bd5aa
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-12-23 21:57:15 -0500

    set up signup modal

--- frontend/mobile/RackApp/src/app/pages/login/login.page.ts
+++ frontend/mobile/RackApp/src/app/pages/login/login.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
+  @ViewChild(IonModal) modal!: IonModal;
+ 
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+  }
+
+  // onWillDismiss(event: Event) {
+  //   const ev = event as CustomEvent<OverlayEventDetail<string>>;
+  //   if (ev.detail.role === 'confirm') {
+  //     this.message = `Hello, ${ev.detail.data}!`;
+  //   }
+  // }
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
