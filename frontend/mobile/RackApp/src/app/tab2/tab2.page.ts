730cffe6c68ec0e2a7220421797693bdb0daad5e
paulaapark <99138656+paulaapark@users.noreply.github.com>
2023-01-17 11:24:49 -0500

    trying to toggle filter bar still

--- frontend/mobile/RackApp/src/app/tab2/tab2.page.ts
+++ frontend/mobile/RackApp/src/app/tab2/tab2.page.ts
@@ -11,21 +11,21 @@ import { RackService } from '../services/rack.service';
 export class Tab2Page {
 
   userRack:any;
-  ishidden:boolean = true;
+  ishidden!:boolean;
 
   constructor(public rackService:RackService) {}
   ngOnInit(){
     this.rackService.getUserRack().subscribe(res => {
       this.userRack = Object.values(res);
     });
+    this.ishidden = true;
   }  
 
   toggleHidden(){
-    if(this.ishidden=false) {
-      this.ishidden = true;
-    }
-    if(this.ishidden = true){
+     if(this.ishidden = true){
       this.ishidden = false;
+    } else {
+      this.ishidden = true;
     };
   }
 
