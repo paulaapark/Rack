a02f79353556ce64a45ce38450e4ac0fa128addc
paulaapark <99138656+paulaapark@users.noreply.github.com>
2023-01-23 14:38:01 -0500

    time of day added to tab3

--- frontend/mobile/RackApp/src/app/tab3/builder-input/builder-input.page.ts
+++ frontend/mobile/RackApp/src/app/tab3/builder-input/builder-input.page.ts
@@ -7,8 +7,19 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./builder-input.page.scss'],
 })
 export class BuilderInputPage implements OnInit {
+  public myDate = new Date();
+  public hrs = this.myDate.getHours();
+  public timeOfDay:string;
 
-  constructor(public userService:UserService) { }
+  constructor(public userService:UserService) { 
+    if (this.hrs < 12){
+      this.timeOfDay = 'morning';
+    }else if ( this.hrs >= 12 && this.hrs <=17){
+      this.timeOfDay = 'afternoon';
+    }else{
+      this.timeOfDay = 'evening';
+    }
+  }
 
   ngOnInit() {
   }
